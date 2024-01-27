f870e95ecc8d594d098d0bd559d2447a3abca4bf
Nishant <crcnish0710@gmail.com>
2024-01-27 16:23:19 +0530

    UI for create listing

--- src/Pages/CreateListing.js
+++ src/Pages/CreateListing.js
@@ -1,9 +1,74 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
 import { Container, Row, Col, Card, Form, InputGroup } from 'react-bootstrap';
 import Header from '../Components/Header';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../GoogleFirebase';
 
 const CreateListing = () => {
+
+  const [files,setFiles]=useState([]);
+  const [filePerc, setFilePerc] = useState(0);
+  const [formData,setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  console.log(formData);
+  const handleImageSubmit = (e)=>{
+    e.preventDefault();
+
+    if(files.length > 0 && files.length+formData.imageUrls.length<7){
+      const promises = [];
+
+      setFilePerc(1);
+      for(let i=0;i<files.length; i++){
+        promises.push(storeImage(files[i]));
+      }
+      
+      Promise.all(promises).then((urls)=>{
+        setFormData({...formData,imageUrls:formData.imageUrls.concat(urls)});
+        setImageUploadError(false);
+        setFilePerc(0);
+      }).catch((err)=>{
+        setImageUploadError('Image upload error (can upload 1-6)');
+        setFilePerc(0);
+      })
+      
+    }
+    else{
+      setImageUploadError('You can upload between 1-6 images');
+    }
+  }
+
+  const storeImage = async (file)=>{
+    return new Promise((resolve,reject)=>{
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    })
+  }
+
+  const handleRemoveImage = (index) =>{
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_,i)=> i!==index),
+    });
+  }
   return (
     <div>
       <Header />
@@ -126,11 +191,37 @@ const CreateListing = () => {
                 </p>
                   <div >
                     <div style={{ display: 'inline-block', border: '1px solid #ccc', padding: '10px', borderRadius: '5px', marginRight: '2px',width:'65%' }}>
-                        <input type='file' accept='image/*' multiple/>
+                        <input  onChange={(e)=>{
+                          setFiles(e.target.files);
+                        }} type='file' accept='image/*' multiple/>
                     </div>
-                    <button className="btn btn-primary" style={{padding: '10px',width:'30%'}}>UPLOAD</button>
+                    <button className="btn btn-primary" style={{padding: '10px',width:'33%'}} onClick={handleImageSubmit}>UPLOAD</button>
                     </div>
+                      <p>
+                      {filePerc > 0 && filePerc < 100 && (
+                        <span>Uploading ...</span>
+                      ) }
+                      </p>
 
+                      {formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+                      <div key={url} style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '10px', marginBottom: '10px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                          <img
+                            src={url}
+                            alt='listing image'
+                            style={{ width: '100px', height: '100px', marginRight: '10px' }}
+                          />
+                          <button
+                          
+                            className="btn btn-danger btn-sm"
+                            style={{ padding: '5px' }}
+                            onClick={()=>handleRemoveImage(index)}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    ))}
                     <button
                       className='w-100 btn btn-lg mt-4 ml-0 mb-4 mr-4'
                       style={{
@@ -151,6 +242,24 @@ const CreateListing = () => {
           </Col>
         </Row>
       </Container>
+      {imageUploadError && (
+      <p
+        className='text-red-500'
+        style={{
+          position: 'fixed',
+                  bottom: '0',
+                  left: '0',
+                  width: '100%',
+                  textAlign: 'center',
+                  backgroundColor: '#f8d7da',
+                  padding: '10px',
+                  color: 'red',
+                  margin: '0',
+        }}
+      >
+        {imageUploadError}
+      </p>
+    )}
     </div>
   );
 };
